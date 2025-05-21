0fae3cf68f7e22d877bb59350156e0ef81ae6aad
yevheniiskakun <yevhenii.skakun@gmail.com>
2025-05-21 21:57:03 +0200

    Creating Debian vm. Added gitignore and how to generate public key in readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hashicorp-terraform-azure
+
+<strong>To generate public key</strong> <code>ssh-keygen -t rsa -b 2048 -f id_rsa</code>
---
+++ vm.tf
@@ -0,0 +1,65 @@
+data "azurerm_platform_image" "openwebui" {
+  location  = azurerm_resource_group.openwebui.location
+  publisher = "Debian"
+  offer     = "debian-11"
+  sku       = "11"
+}
+
+resource "azurerm_resource_group" "openwebui" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "openwebui" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.openwebui.location
+  resource_group_name = azurerm_resource_group.openwebui.name
+}
+
+resource "azurerm_subnet" "openwebui" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.openwebui.name
+  virtual_network_name = azurerm_virtual_network.openwebui.name
+  address_prefixes     = [cidrsubnet(azurerm_virtual_network.openwebui.address_space[0], 8, 2)]
+}
+
+resource "azurerm_network_interface" "openwebui" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.openwebui.location
+  resource_group_name = azurerm_resource_group.openwebui.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.openwebui.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "openwebui" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.openwebui.name
+  location            = azurerm_resource_group.openwebui.location
+  size                = "Standard_A2_v2"
+  admin_username      = "openwebui"
+  network_interface_ids = [
+    azurerm_network_interface.openwebui.id,
+  ]
+
+  admin_ssh_key {
+    username   = "openwebui"
+    public_key = file("public_keys/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = data.azurerm_platform_image.openwebui.publisher
+    offer     = data.azurerm_platform_image.openwebui.offer
+    sku       = data.azurerm_platform_image.openwebui.sku
+    version   = data.azurerm_platform_image.openwebui.version
+  }
+}
